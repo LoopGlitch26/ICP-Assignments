3f9d0f4e3d21341c0878a2239d28d07b2f6c340c
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-03 11:20:24 +0530

    Add files via upload

---
+++ A1Q1.java
@@ -0,0 +1,20 @@
+//Write a java program to display following messages.
+//Hello World!
+//Hello Again
+//I like typing this.
+//This is fun.
+//Yay! Printing.
+//I'd much rather you 'not'.
+//I "said" do not touch this.
+
+public class A1Q1 {
+public static void main(String[] args){
+System.out.println("Hello World!");
+System.out.println("Hello Again");
+System.out.println("I like typing this.");
+System.out.println("This is fun.");
+System.out.println("Yay! Printing.");
+System.out.println("I'd much rather you 'not'.");
+System.out.println("I \"said\" do not touch this.");
+}
+}
