ecf856ef586a9c2caaf056f1b26578f6b75790f0
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-03 11:21:17 +0530

    Add files via upload

---
+++ A1Q5.java
@@ -0,0 +1,11 @@
+//Write a java program that stores your Regd. No and year of admission into two variables,
+//and displays their values on the screen.
+//My Regd. No is 191123142 and I have taken admission in B. Tech. In 2019.
+
+public class A1Q5 {
+public static void main(String[] args) {
+int regd=191123142;
+int year=2019;
+System.out.println("My Regd. No is "+regd+ " and I have taken admission in B. Tech. In "+year+".");
+}
+}
