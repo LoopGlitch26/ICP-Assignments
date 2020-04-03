a921f1930b4c81b00a12640da8b3e84f973ea7de
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-03 11:21:53 +0530

    Add files via upload

---
+++ A1Q6.java
@@ -0,0 +1,14 @@
+//Let we have two values 113, 2.71828. Then, declare two different variables to hold the given
+//values. Write the java program to display the values of these two variables on the screen, one
+//per line.
+//This is room # 113
+//e is close to 2.71828
+
+public class A1Q6 {
+public static void main(String[] args) {
+double e=2.71828;
+int a=113;
+System.out.println("This is room # "+a);
+System.out.println("e is close to "+e);
+}
+}
